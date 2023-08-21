9fe3f7963414bbbdf280acbfc344780c506bca47
nikhil.thakur <nikhil.thakur@altruistindia.com>
2023-08-21 16:56:27 +0530

    added some more apis

--- routes/HomeCarousel.js
+++ routes/HomeCarousel.js
@@ -553,7 +553,7 @@ router.post("/getmail", async (req, res) => {
     await doc.loadInfo();
 
     // Index of the sheet
-    let sheet = doc.sheetsByIndex[8];
+    let sheet = doc.sheetsByIndex[7];
 
     // Get all the rows
     await sheet.addRow(req.body);
@@ -562,6 +562,198 @@ router.post("/getmail", async (req, res) => {
   } catch (error) {
     console.error("Error adding data:", error);
     res.status(500).json({ error: "An error occurred while adding data" });
+  }
+});
+router.get("/updates", async (req, res) => {
+  try {
+    const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
+    const timestamp = new Date().toISOString();
+    // use service account creds
+    await doc.useServiceAccountAuth({
+      client_email: CREDENTIALS.client_email,
+      private_key: CREDENTIALS.private_key,
+    });
+
+    // load the documents info
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[9];
+
+    // Get all the rows
+    let rows = await sheet.getRows();
+
+    if (rows?.length === 0) {
+      const responseWrapper = new ResponseWrapper();
+      const s6tatusDescription = new StatusDescription();
+      responseWrapper.setStatusDescription(s6tatusDescription);
+      s6tatusDescription.setStatusCode(201);
+      s6tatusDescription.setStatusMessage("No Data Found");
+      return res.status(200).json({ responseWrapper });
+    }
+
+    const headerRow = await sheet.headerValues;
+    if (rows?.length > 0) {
+      const responseWrapper = new ResponseWrapper();
+      const s6tatusDescription = new StatusDescription();
+      responseWrapper.setStatusDescription(s6tatusDescription);
+      s6tatusDescription.setStatusCode(200);
+      s6tatusDescription.setStatusMessage("Success");
+      let data = [];
+
+      for (let index = 0; index < rows.length; index++) {
+        const element = rows[index];
+
+        data.push(element["_rawData"]);
+      }
+
+      const arrayOfObjects = data.map((row) => {
+        const obj = {};
+        for (let i = 0; i < headerRow?.length; i++) {
+          obj[headerRow[i]] = row[i];
+        }
+        return obj;
+      });
+
+      responseWrapper.setData(arrayOfObjects);
+      res.status(200).json({ responseWrapper });
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    const responseWrapper = new ResponseWrapper();
+    const s6tatusDescription = new StatusDescription();
+    responseWrapper.setStatusDescription(s6tatusDescription);
+    s6tatusDescription.setStatusCode(500);
+    s6tatusDescription.setStatusMessage("Internal Server Error");
+    res.status(500).json(responseWrapper);
+  }
+});
+router.get("/counter", async (req, res) => {
+  try {
+    const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
+    const timestamp = new Date().toISOString();
+    // use service account creds
+    await doc.useServiceAccountAuth({
+      client_email: CREDENTIALS.client_email,
+      private_key: CREDENTIALS.private_key,
+    });
+
+    // load the documents info
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[11];
+
+    // Get all the rows
+    let rows = await sheet.getRows();
+
+    if (rows?.length === 0) {
+      const responseWrapper = new ResponseWrapper();
+      const s6tatusDescription = new StatusDescription();
+      responseWrapper.setStatusDescription(s6tatusDescription);
+      s6tatusDescription.setStatusCode(201);
+      s6tatusDescription.setStatusMessage("No Data Found");
+      return res.status(200).json({ responseWrapper });
+    }
+
+    const headerRow = await sheet.headerValues;
+    if (rows?.length > 0) {
+      const responseWrapper = new ResponseWrapper();
+      const s6tatusDescription = new StatusDescription();
+      responseWrapper.setStatusDescription(s6tatusDescription);
+      s6tatusDescription.setStatusCode(200);
+      s6tatusDescription.setStatusMessage("Success");
+      let data = [];
+
+      for (let index = 0; index < rows.length; index++) {
+        const element = rows[index];
+
+        data.push(element["_rawData"]);
+      }
+
+      const arrayOfObjects = data.map((row) => {
+        const obj = {};
+        for (let i = 0; i < headerRow?.length; i++) {
+          obj[headerRow[i]] = row[i];
+        }
+        return obj;
+      });
+
+      responseWrapper.setData(arrayOfObjects);
+      res.status(200).json({ responseWrapper });
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    const responseWrapper = new ResponseWrapper();
+    const s6tatusDescription = new StatusDescription();
+    responseWrapper.setStatusDescription(s6tatusDescription);
+    s6tatusDescription.setStatusCode(500);
+    s6tatusDescription.setStatusMessage("Internal Server Error");
+    res.status(500).json(responseWrapper);
+  }
+});
+router.get("/navcontent", async (req, res) => {
+  try {
+    const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
+    const timestamp = new Date().toISOString();
+    // use service account creds
+    await doc.useServiceAccountAuth({
+      client_email: CREDENTIALS.client_email,
+      private_key: CREDENTIALS.private_key,
+    });
+
+    // load the documents info
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[10];
+
+    // Get all the rows
+    let rows = await sheet.getRows();
+
+    if (rows?.length === 0) {
+      const responseWrapper = new ResponseWrapper();
+      const s6tatusDescription = new StatusDescription();
+      responseWrapper.setStatusDescription(s6tatusDescription);
+      s6tatusDescription.setStatusCode(201);
+      s6tatusDescription.setStatusMessage("No Data Found");
+      return res.status(200).json({ responseWrapper });
+    }
+
+    const headerRow = await sheet.headerValues;
+    if (rows?.length > 0) {
+      const responseWrapper = new ResponseWrapper();
+      const s6tatusDescription = new StatusDescription();
+      responseWrapper.setStatusDescription(s6tatusDescription);
+      s6tatusDescription.setStatusCode(200);
+      s6tatusDescription.setStatusMessage("Success");
+      let data = [];
+
+      for (let index = 0; index < rows.length; index++) {
+        const element = rows[index];
+
+        data.push(element["_rawData"]);
+      }
+
+      const arrayOfObjects = data.map((row) => {
+        const obj = {};
+        for (let i = 0; i < headerRow?.length; i++) {
+          obj[headerRow[i]] = row[i];
+        }
+        return obj;
+      });
+
+      responseWrapper.setData(arrayOfObjects);
+      res.status(200).json({ responseWrapper });
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    const responseWrapper = new ResponseWrapper();
+    const s6tatusDescription = new StatusDescription();
+    responseWrapper.setStatusDescription(s6tatusDescription);
+    s6tatusDescription.setStatusCode(500);
+    s6tatusDescription.setStatusMessage("Internal Server Error");
+    res.status(500).json(responseWrapper);
   }
 });
 
